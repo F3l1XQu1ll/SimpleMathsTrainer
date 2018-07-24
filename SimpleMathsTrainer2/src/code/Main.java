da2f8926969c866231a25b8c55b6ff1fb0278665
Nguyen <Nguyen@192.168.0.10>
2018-07-24 17:40:28 +0200

    new newer newest code

--- SimpleMathsTrainer2/src/code/Main.java
+++ SimpleMathsTrainer2/src/code/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//ich bin dämlich.
-		System.out.println("Helo World!");
+		//ich bin dämlich. man muss immer ctrl + s drücken...........
+		System.out.println("Hello World!");
 	}
 
 }
