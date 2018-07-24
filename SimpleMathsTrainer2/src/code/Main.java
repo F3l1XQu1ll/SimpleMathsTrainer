dd80982f3bce7edadd287d365776863731956a72
Nguyen <Nguyen@192.168.0.10>
2018-07-24 18:33:16 +0200

    new new new code new newer

--- SimpleMathsTrainer2/src/code/Main.java
+++ SimpleMathsTrainer2/src/code/Main.java
@@ -5,48 +5,48 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
-	// ich bin dämlich. man muss immer ctrl + s drücken...........
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// I am dumb. ALWAYS press ctrl + s before uploading the newest code
 
-	System.out.println("Hello World!");
-	int a = (int) (1 + Math.random() * 10);
-	int b = (int) (1 + Math.random() * 10);
+		System.out.println("Hello World!");
+		int a = (int) (1 + Math.random() * 10);
+		int b = (int) (1 + Math.random() * 10);
 
-	Random rand = new Random();
-	char operator_Char = 0;
-	int op = rand.nextInt(4);
+		Random rand = new Random();
+		char operator_Char = 0;
+		int op = rand.nextInt(4);
 
-	switch (op) {
-	case 0:
-	    operator_Char = '+';
-	    break;
-	case 1:
-	    operator_Char = '-';
-	    break;
-	case 2:
-	    operator_Char = '*';
-	    break;
-	case 3:
-	    operator_Char = '/';
-	    break;
+		switch (op) {
+		case 0:
+			operator_Char = '+';
+			break;
+		case 1:
+			operator_Char = '-';
+			break;
+		case 2:
+			operator_Char = '*';
+			break;
+		case 3:
+			operator_Char = '/';
+			break;
+		}
+
+		double input = 0;
+
+		System.out.println("Rechnen Sie " + a + operator_Char + b + " aus.");
+		Scanner keyboard = new Scanner(System.in);
+		input = keyboard.nextDouble();
+
+		if (a + b == input) {
+
+			System.out.println("Richtig :D");
+
+		}
+		if (a + b != input) {
+
+			System.out.println("FALSCH!");
+		}
+		keyboard.close();
 	}
-
-	int input = 0;
-
-	System.out.println("Rechnen Sie " + a + operator_Char + b + " aus.");
-	Scanner keyboard = new Scanner(System.in);
-	input = keyboard.nextInt();
-
-	if (a + b == input) {
-
-	    System.out.println("Richtig :D");
-
-	}
-	if (a + b != input) {
-
-	    System.out.println("FALSCH!");
-	}
-	keyboard.close();
-    }
 }
