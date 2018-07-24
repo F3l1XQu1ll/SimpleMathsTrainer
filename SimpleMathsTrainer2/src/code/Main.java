b06af0f5f0eb283ab27784f5012aba07ee8923bb
F3l1XQu1ll <fakenutzer0001@gmail.com>
2018-07-24 20:26:10 +0200

    fixed check of answers

--- SimpleMathsTrainer2/src/code/Main.java
+++ SimpleMathsTrainer2/src/code/Main.java
@@ -6,15 +6,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
 	// I am dumb. ALWAYS press ctrl + s before uploading the newest code
 
 	System.out.println("Hello Calculators World!");
+
+	/**
+	 * initialize new Random
+	 */
 	Random rand = new Random();
+
+	/**
+	 * generate new operands
+	 */
 	double a = rand.nextInt(10);
 	double b = rand.nextInt(10);
-	//pathed
-	
+
+	/**
+	 * generate the operator
+	 */
 	char operator_Char = 0;
 	int op = rand.nextInt(4);
 
@@ -35,19 +44,39 @@ public class Main {
 
 	double input = 0;
 
-	System.out.println("Rechnen Sie " + a + operator_Char + b + " aus.");
+	System.out.println("Rechnen Sie " + a + " " + operator_Char + " " + b + " aus.");
 	Scanner keyboard = new Scanner(System.in);
 	input = keyboard.nextDouble();
 
-	if (a + b == input) {
-
-	    System.out.println("Richtig :D");
-
-	}
-	if (a + b != input) {
-
-	    System.out.println("FALSCH!");
+	/**
+	 * check users answer
+	 */
+	switch (op) {
+	case 0:
+	    showReply(input == a + b);
+	    break;
+	case 1:
+	    showReply(input == a - b);
+	    break;
+	case 2:
+	    showReply(input == a * b);
+	    break;
+	case 3:
+	    showReply(input == a / b);
 	}
 	keyboard.close();
+    }
+
+    /**
+     * show a message to the user depending on was the given answer correct
+     * 
+     * @param isCorrect
+     *                      defines if the given answer was correct
+     */
+    private static void showReply(boolean isCorrect) {
+	if (isCorrect)
+	    System.out.println("Richtig :D");
+	else
+	    System.out.println("FALSCH!");
     }
 }
