02d0730e5cdddba96be74b2ace4e3e8de1ef25bb
F3l1XQu1ll <fakenutzer0001@gmail.com>
2018-07-27 17:42:29 +0200

    Commented the LogEvaluator.java

--- SimpleMathsTrainer2/src/code/LogEvaluater.java
+++ SimpleMathsTrainer2/src/code/LogEvaluater.java
@@ -7,14 +7,27 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class LogEvaluater {
+    /**
+     * the path to the logs
+     */
     private Path log_path = Paths.get(System.getProperty("user.home"), "SimpleMathsTrainer", "log.txt");
+
+    /**
+     * new Buffered Reader for reading the logs
+     */
     private BufferedReader reader;
 
+    /**
+     * to store the errors and the correct answers count
+     */
     private double add_e = 0, add_c = 0;
     private double sub_e = 0, sub_c = 0;
     private double mul_e = 0, mul_c = 0;
     private double div_e = 0, div_c = 0;
 
+    /**
+     * constructor to initialize the new BufferedReader
+     */
     public LogEvaluater() {
 	try {
 	    reader = Files.newBufferedReader(log_path);
@@ -24,18 +37,40 @@ public class LogEvaluater {
 	}
     }
 
+    /**
+     * method for output
+     */
     public void dump() {
+	/**
+	 * store the current line read by the reader
+	 */
 	String current_line = null;
 	try {
 	    /**
-	     * read all lines
+	     * read all lines while the next line isn't empty (and store read line, because
+	     * reader can just read each line one time, not again)
 	     */
 	    while ((current_line = reader.readLine()) != null) {
+		/**
+		 * store all parts of the current read line, split at each ";"
+		 */
 		String[] content = current_line.split(";");
-		if (content[0] != "started new training;2018-07-26T18:00:10.180134"
-			&& content[0] != "new Try;2018-07-26T18:00:22.880847") {
+
+		/**
+		 * just read the relevant lines (not these who signal a new training or a new
+		 * try)
+		 */
+		if (content[0] != "started new training" && content[0] != "new Try") {
+		    /**
+		     * get the 2nd part of each line (which is showing if the calculation was an
+		     * addition, subtraction, multiplication or division)
+		     */
 		    switch (content[1]) {
 		    case "0":
+			/**
+			 * if the answer wasn't right, add 1 to the errors, if not add 1 to the corrects
+			 * (in this case to the additions)
+			 */
 			if (!content[0].equals("Correct Answer")) {
 			    add_e++;
 			} else {
@@ -71,6 +106,10 @@ public class LogEvaluater {
 	    e.printStackTrace();
 	}
 
+	/**
+	 * print the results (the "\t" prints an tabulator) The average is only
+	 * calculated, if the count of answers is bigger than 0
+	 */
 	System.out.println("\t\tError\tCorrect");
 	System.out.println("Addition:\t" + add_e + "\t" + add_c);
 	if ((add_c + add_e) != 0)
@@ -95,10 +134,6 @@ public class LogEvaluater {
 	    System.out.println("Average:\t" + div_e / (div_c + div_e));
 	else
 	    System.out.println("You have calculated nothing yet ...");
-	
-	/**
-	 * tray again
-	 */
     }
 
 }
