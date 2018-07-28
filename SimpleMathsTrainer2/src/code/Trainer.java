61deefc14325a2bce020737c5df11f66c4ecf18a
F3l1XQu1ll <fakenutzer0001@gmail.com>
2018-07-28 12:41:01 +0200

    trying to fix conflicts

--- SimpleMathsTrainer2/src/code/Trainer.java
+++ SimpleMathsTrainer2/src/code/Trainer.java
@@ -17,6 +17,7 @@ public class Trainer {
     private Scanner keyboard = new Scanner(System.in);
 
     private DateTimeFormatter d_t_formater = DateTimeFormatter.ISO_DATE_TIME;
+    
     private char operator_Char = 0;
 
     /**
